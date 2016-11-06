0c727815664dbbdff4772d11223a103350bce4d9
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-11-06 22:20:28 +0300

    Receipt setting prefix

---
+++ Config/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return [
+    'receipt_prefix' => [
+        'description' => 'Receipt Prefix',
+        'view' => 'text',
+        'hint' => 'A 3-digit code added to receipt-number'
+    ],
+];
