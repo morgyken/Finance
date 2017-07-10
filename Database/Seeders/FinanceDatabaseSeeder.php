a66bc3004ed94bc73f266cdea4a66dca003c1e31
Samuel Dervis <samueldervis@gmail.com>
2017-07-10 16:57:47 +0300

    Tweak migrations

--- Database/Seeders/FinanceDatabaseSeeder.php
+++ Database/Seeders/FinanceDatabaseSeeder.php
@@ -4,15 +4,18 @@ namespace Ignite\Finance\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class FinanceDatabaseSeeder extends Seeder {
+class FinanceDatabaseSeeder extends Seeder
+{
 
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
         $this->call(GLTableSeeder::class);
+        $this->call(SmartInterfaceTableSeeder::class);
         // $this->call("OthersTableSeeder");
     }
 
