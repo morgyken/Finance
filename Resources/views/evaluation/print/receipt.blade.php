539b1237cbec71260e2c932f7e8275ba8a035dfc
bravo kiptoo <bkiptoo@collabmed.com>
2017-08-25 18:00:52 +0300

    Show clinic or facility

--- Resources/views/evaluation/print/receipt.blade.php
+++ Resources/views/evaluation/print/receipt.blade.php
@@ -120,24 +120,22 @@ function getAmount($sales) {
         </center>
         @endif
         <div class="box-header with-border">
-            @if(!empty($clinic))
             <p style="font-size: 90%; <?php if (!isset($a4)) { ?> text-align: center<?php } ?>">
+                @if($clinic->address !==''||$clinic->town!=='')
                 P.O Box {{$clinic->address}}, {{$clinic->town}}.<br/>
                 Visit us: {{$clinic->location}}<br>
                 {{$clinic->street}}<br>
                 Email: {{$clinic->email}}<br>
                 Call Us: {{$clinic->mobile}}
                 <br/> {{$clinic->telephone?"Or: ".$clinic->telephone:''}}<br>
-            </p>
-            @else
-            <p style="font-size: 90%; <?php if (!isset($a4)) { ?> text-align: center<?php } ?>">
+                @else
                 P.O Box {{config('practice.address')}}, {{config('practice.town')}}.<br/>
                 Visit us: {{config('practice.building')?config('practice.building').',':''}}<br>
                 {{config('practice.street')?config('practice.street').',':''}}<br><br>
                 Email: {{config('practice.email')}}<br>
                 {{config('practice.telephone')?'Call Us:- '.config('practice.telephone'):''}}<br>
+                @endif
             </p>
-            @endif
         </div>
         <div class="box-body">
             <div class="col-md-12">
