46e4d854fdc468f52a3873092c0f5d5106408778
David Ngugi <ndavidngugi@gmail.com>
2017-09-20 00:40:56 +0300

    Move migrations for Removed Bills to Finance Module

---
+++ Database/Migrations/2017_09_20_003842_AddWardFieldToRemovedBillsTable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWardFieldToRemovedBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('finance_removed_bills', function (Blueprint $table) {
+            $table->integer('ward')->nullable(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
