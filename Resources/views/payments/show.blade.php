f14f363b4b946ec69929d5b5e35dea511285b207
kisiara <kisiarafrancis@gmail.com>
2017-11-18 17:28:00 +0000

    modifying the payments process

--- Resources/views/payments/show.blade.php
+++ Resources/views/payments/show.blade.php
@@ -27,27 +27,48 @@
                 <hr>
 
                 <div class="row"> 
-                    @foreach($modes as $mode => $detail)
-                        <div class="col-md-6">
-                            <h4>{{ ucwords($mode) }} Payment</h4>
+                    @if($modes->count() > 0)
+                        @foreach($modes as $mode => $detail)
+                            <div class="col-md-6">
+                                <h4>{{ ucwords($mode) }} Payment</h4>
 
-                            <p>Amount Paid: <code>kshs. {{ number_format($detail->amount) }}</code></p>
+                                <p>Amount Paid: <code>kshs. {{ number_format($detail->amount) }}</code></p>
+                            </div>
+                        @endforeach
+                    @else
+                        <div class="col-md-12">
+                            <div class="jumbotron col-md-12">
+                                <h4 class="text-center">Sorry! No deposits were made</h4> 
+                            </div>
                         </div>
-                    @endforeach
+                    @endif    
                 </div>
 
                 <hr>
 
-                <div class="row"> 
-                    <div class="col-md-5">
-                        <h4><b>DEPOSIT TOTAL kshs. {{ $total }}</b></h4>
+                @if($modes->count() > 0)
+                    <div class="row"> 
+                        <div class="col-md-5">
+                            <h4><b>DEPOSIT TOTAL kshs. {{ $total }}</b></h4>
+                        </div>
+                        <div class="col-md-7">
+                            <button class="col-md-2 col-md-offset-1 btn btn-sm btn-primary">Print Thermal</button>
+                            <button class="col-md-2 col-md-offset-1 btn btn-sm btn-primary">Print A4</button>
+                        </div>
                     </div>
+                    <hr>
+                @endif
 
-                    <div class="col-md-7">
-                        <button class="col-md-2 col-md-offset-1 btn btn-sm btn-primary pull-right">Print Thermal</button>
-                        <button class="col-md-2  btn btn-sm btn-primary pull-right">Print A4</button>
-                    </dv>
+                @if(is_module_enabled('Inpatient'))
+                <div class="row"> 
+                    <div class="col-md-12">
+                        <a target="_blank" href="{{ url('/inpatient/admission-letter/create/'. $payment->patients->id) }}" class="col-md-2 btn btn-sm btn-success">
+                            Print Admission Letter
+                        </a>
+                    </div>
                 </div>
+                @endif
+
             </div>
 
             
