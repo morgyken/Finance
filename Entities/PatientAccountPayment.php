7c08972ac5eefb8be756f77d34f5705f4c07d62a
Samuel Dervis <samueldervis@gmail.com>
2017-11-30 18:12:29 +0300

    Add patient account model

---
+++ Entities/PatientAccountPayment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ignite\Finance\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PatientAccountPayment extends Model
+{
+    protected $guarded = [];
+    protected $table = 'finance_patient_account_payments';
+}
